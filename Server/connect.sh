9e3c8701a8c4daf970d0ddb6b901d23000c32caa
gdake <gdake@github.com>
2020-10-23 09:32:39 -0500

    1.0.9RC1 mods

---
+++ Server/connect.sh
@@ -0,0 +1,1 @@
+while true; do ssh -R 25565:localhost:25565 142.93.124.124 && break; done
--- Server/start_server2.sh
+++
@@ -1,1 +0,0 @@
-java -jar -Xms1G -Xmx4G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch forge-1.16.3-34.1.16.jar nogui
